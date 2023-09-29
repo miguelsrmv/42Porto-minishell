f0dc996ee1a43c0cb2a705243f63edd031ea07c9
Miguel <miguel.srmv@gmail.com>
2023-09-29 16:55:04 +0100

    Libft added

---
+++ src/libft/ft_swap.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_swap.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mde-sa-- <mde-sa--@student.42porto.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/17 22:01:22 by mde-sa--          #+#    #+#             */
+/*   Updated: 2023/09/25 18:33:30 by mde-sa--         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+#include <string.h>
+#include "libft.h"
+
+void	ft_swap(void *a, void *b, size_t size)
+{
+	void	*temp;
+
+	temp = malloc(size);
+	if (temp == NULL)
+		return ;
+	ft_memcpy(temp, a, size);
+	ft_memcpy(a, b, size);
+	ft_memcpy(b, temp, size);
+	free(temp);
+}
