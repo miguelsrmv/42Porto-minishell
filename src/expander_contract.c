d43a019d64c133757073eb2b06cb9245ac761520
Miguel <miguel.srmv@gmail.com>
2023-11-15 11:06:05 +0000

    Midway through expansion

--- src/expander_contract.c
+++ src/expander_contract.c
@@ -6,7 +6,7 @@
 /*   By: mde-sa-- <mde-sa--@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/10 10:13:14 by mde-sa--          #+#    #+#             */
-/*   Updated: 2023/11/10 10:13:23 by mde-sa--         ###   ########.fr       */
+/*   Updated: 2023/11/10 16:33:59 by mde-sa--         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,51 +14,44 @@
 
 void	contract_double_vector(char **vector)
 {
-	int		vector_index;
-	int		i;
-	int		j;
-	char	quote_status;
-	char	*result;
-	char	*temp;
+	int	vector_index;
 
 	vector_index = 0;
 	while (vector[vector_index])
 	{
-		result = (char *)malloc(sizeof(vector[vector_index]));
-		if (!result)
-			return ;
-		quote_status = '\0';
-		i = 0;
-		j = 0;
-		while ((vector[vector_index])[i])
-		{
-			if (ft_isquote((vector[vector_index])[i]))
-			{
-				quote_status = vector[vector_index][i];
-				i++;
-			}
-			if (quote_status)
-			{
-				while (vector[vector_index][i] != quote_status && vector[vector_index][i])
-					result[j++] = vector[vector_index][i++];
-				quote_status = '\0';
-				i++;
-			}
-			else if (!quote_status)
-			{
-				while (!quote_status && (vector[vector_index][i]))
-				{
-					result[j++] = vector[vector_index][i++];
-					if (ft_isquote(vector[vector_index][i]))
-						quote_status = vector[vector_index][i];
-				}
-			}
-		}
-		j++;
-		result[j] = '\0';
-		temp = vector[vector_index];
-		vector[vector_index] = result;
-		free(temp);
+		if (ft_strchr(vector[vector_index], squote)
+			|| ft_strchr(vector[vector_index], dquote))
+			vector[vector_index] = take_out_quotes(vector[vector_index]);
 		vector_index++;
 	}
+}
+
+char	*take_out_quotes(char *string)
+{
+	char	*result;
+	int		string_len;
+	int		i;
+	int		j;
+	char	quote_flag;
+
+	string_len = (int)ft_strlen(string);
+	result = (char *)malloc(string_len + 1);
+	if (!result)
+		return (NULL);
+	i = 0;
+	j = 0;
+	quote_flag = '\0';
+	while (string[i])
+	{
+		if (ft_isquote(string[i]) && quote_flag == '\0')
+			quote_flag = string[i];
+		else if (ft_isquote(string[i]) && string[i] == quote_flag)
+			quote_flag = '\0';
+		if (!ft_isquote(string[i]) || (quote_flag == '\0'))
+			result[j++] = string[i];
+		i++;
+	}
+	while (j < string_len)
+		result[j++] = '\0';
+	return (result);
 }
--- src/expander_env.c
+++ src/expander_env.c
@@ -6,7 +6,7 @@
 /*   By: mde-sa-- <mde-sa--@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/24 14:23:38 by mde-sa--          #+#    #+#             */
-/*   Updated: 2023/11/10 12:36:04 by mde-sa--         ###   ########.fr       */
+/*   Updated: 2023/11/15 11:05:58 by mde-sa--         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,21 +22,46 @@ int	is_valid_env_char(char c)
 char	*concatenate_env_substrings(char *left, char *env, char *right,
 			char *string)
 {
+	char	*expanded;
 	char	*result;
 	char	*temp;
 
-	result = ft_strjoin(left, getenv(env));
+	if (ft_strchr(env, squote) || ft_strchr(env, dquote))
+	{
+		expanded = take_out_quotes(env);
+		result = ft_strjoin(left, expanded);
+		free (expanded);
+	}
+	else
+		result = ft_strjoin(left, getenv(env));
 	temp = result;
 	result = ft_strjoin(result, right);
-	free(left);
-	free(right);
 	free(temp);
 	free(string);
+	free(left);
+	free(env);
+	free(right);
 	if (result == NULL)
 		return (ft_strdup("\0"));
 	return (result);
 }
 
+// Além da expansão, fazer concatenate de left + esta substring + right e atualizar *start!!!....
+void	expand(char **string, int *start, char quote_flag)
+{
+	if (!(*string)[(*start) + 1])
+		expand_to_dollar_sign(string, start);
+	else if (!quote_flag && (*string)[(*start) + 1] != squote)
+		expand_env(string, start);
+	else if (!quote_flag && (*string)[(*start) + 1] == squote)
+		take_out_after_quotes(string, start);
+	else if (quote_flag && (*string)[(*start) + 1] != squote)
+		expand_env(string, start);
+	else if (quote_flag && (*string)[(*start) + 1] == squote)
+		take_out_quote_flag(string, start);
+}
+
+/*
 char	*expand_env(char *string, int start)
 {
 	char	*left;
@@ -44,42 +69,51 @@ char	*expand_env(char *string, int start)
 	char	*right;
 	int		i;
 
-	i = start;
-	left = ft_substr(string, 0, i);
-	i++;
-	while (is_valid_env_char(string[i]) && string[i])
+	if (!ft_strchr(string, squote) && !ft_strchr(string, dquote))
+	{
+		i = start;
+		left = ft_substr(string, 0, i);
 		i++;
-	env = ft_substr(string, ft_strlen(left) + 1,
-			i - ft_strlen(left) - 1);
-	right = ft_substr(string, i, ft_strlen(string) - i);
-	string = concatenate_env_substrings(left, env, right, string);
-	i = -1;
+		while (is_valid_env_char(string[i]) && string[i])
+			i++;
+		env = ft_substr(string, ft_strlen(left) + 1,
+				i - ft_strlen(left) - 1);
+		right = ft_substr(string, i, ft_strlen(string) - i);
+		string = concatenate_env_substrings(left, env, right, string);
+	}
+	else
+	{
+		env = ft_substr(string, 1, ft_strlen(string) - 1);
+		string = env;
+		free (env);
+	}
 	return (string);
 }
+*/
 
 void	expand_double_vector(char **vector)
 {
 	int		vector_index;
 	int		i;
-	char	quote_status;
+	char	quote_flag;
 
 	vector_index = 0;
 	while (vector[vector_index])
 	{
 		i = 0;
-		quote_status = '\0';
+		quote_flag = '\0';
 		while ((vector[vector_index])[i])
 		{
-			if (ft_isquote((vector[vector_index])[i]) && quote_status == '\0')
-				quote_status = vector[vector_index][i++];
-			while (quote_status == squote && vector[vector_index][i] != squote
+			if (ft_isquote((vector[vector_index])[i]) && quote_flag == '\0')
+				quote_flag = vector[vector_index][i++];
+			while (quote_flag == squote && vector[vector_index][i] != squote
 					&& vector[vector_index][i])
 				i++;
 			if (ft_isquote((vector[vector_index])[i])
-				&& vector[vector_index][i] == quote_status)
-				quote_status = '\0';
+				&& vector[vector_index][i] == quote_flag)
+				quote_flag = '\0';
 			else if (vector[vector_index][i] == '$')
-				vector[vector_index] = expand_env(vector[vector_index], i);
+				expand_env(&vector[vector_index], &i, quote_flag);
 			i++;
 		}
 		vector_index++;
@@ -96,12 +130,9 @@ void	expand_command_table(t_command_table **command_table)
 		expand_double_vector(current->cmd);
 		expand_double_vector(current->full_input);
 		expand_double_vector(current->full_output);
-		//contract_double_vector(current->cmd);
-		//contract_double_vector(current->full_input);
-		//contract_double_vector(current->full_output);
-		current = current->next;
+/*		contract_double_vector(current->cmd);
+		contract_double_vector(current->full_input);
+		contract_double_vector(current->full_output);
+		current = current->next;*/
 	}
 }
-
-/// minishell: echo '$USER'$HOME
-
