101972f3fd02db770dd6b646bd9f607fb9d0986f
Miguel <miguel.srmv@gmail.com>
2023-11-28 15:12:19 +0000

    CLeanup

--- src/main.c
+++ src/main.c
@@ -6,26 +6,11 @@
 /*   By: mde-sa-- <mde-sa--@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/29 15:58:36 by mde-sa--          #+#    #+#             */
-/*   Updated: 2023/11/28 14:49:46 by mde-sa--         ###   ########.fr       */
+/*   Updated: 2023/11/28 15:12:06 by mde-sa--         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
-
-void	free_list(t_token *head)
-{
-	t_token	*current;
-	t_token	*next;
-
-	current = head;
-	while (current)
-	{
-		next = current->next;
-		free(current->token);
-		free(current);
-		current = next;
-	}
-}
 
 int	main(int argc, char **envp)
 {
@@ -39,13 +24,13 @@ int	main(int argc, char **envp)
 	command_table = NULL;
 	memptr.lexer_list = &lexer_list;
 	memptr.command_table = &command_table;
- 	while (TRUE)
+	while (TRUE)
 	{
 		lexer_list = read_readline(memptr);
 		command_table = parse_list(lexer_list, memptr);
 		expand_command_table(&command_table, memptr);
 		prepare_processes(&command_table, envp, memptr);
 		clean_memory(memptr);
- 	}
+	}
 	return (0);
 }
