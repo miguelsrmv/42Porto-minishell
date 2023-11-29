6e25b17d200c9b24223f06d43bba0393e87f49f2
Miguel De Sa  Rodrigues Moura Vieira <mde-sa--@c1r9s3.42porto.com>
2023-11-29 14:54:05 +0000

    42 Quarta-feira

--- src/executer.c
+++ src/executer.c
@@ -6,7 +6,7 @@
 /*   By: mde-sa-- <mde-sa--@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/29 12:12:05 by mde-sa--          #+#    #+#             */
-/*   Updated: 2023/11/29 12:13:22 by mde-sa--         ###   ########.fr       */
+/*   Updated: 2023/11/29 14:53:20 by mde-sa--         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,10 +15,10 @@
 void    execute(t_command_table *current, char **envp)
 {
     if (current->command_type == EXECUTABLE)
-        execve(current->cmd_target, current->cmd, envp); // Transformar em execute, depende se executable ou builtin
+        execve(current->cmd_target, current->cmd, envp);
     else
     {
-        ft_fprintf(STDERR_FILENO, "BUILTIN ALERT\n");
-        return ;
+        ft_fprintf(STDERR_FILENO, "BUILTIN ALERT\n"); // Correr function pointer aqui
+        return ;                                      // Exit value ?  
     }
 }
