72728385e1f657eafcc1617f837f6913314c2116
brunofmsilva11 <brunofmsilva.bs@gmail.com>
2023-11-30 18:45:25 +0000

    .

--- src/executer.c
+++ src/executer.c
@@ -15,14 +15,15 @@
 void	execute(t_command_table *current, char **envp, t_memptr memptr)
 {
 	int	(*function_pointer)(char **);
-	int	exit_value;
+	//int	exit_value;
 
 	if (current->command_type == EXECUTABLE)
 		execve(current->cmd_target, current->cmd, envp);
 	else if (current->command_type == BUILTIN)
 	{
 		function_pointer = (int (*)(char **))current->builtin_pointer;
-		exit_value = function_pointer(current->cmd);
+		//exit_value = function_pointer(current->cmd);
+		function_pointer(current->cmd);
 		clean_memory(memptr);
 		//exit(exit_value);
 	}
