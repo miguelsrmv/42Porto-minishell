5523f63ecbedc63afbc2f44fa5ca21f29aca6de4
brunofmsilva11 <brunofmsilva.bs@gmail.com>
2024-02-14 16:07:53 +0000

    norm export4 e get_set

--- src/builtins/get_set.c
+++ src/builtins/get_set.c
@@ -6,7 +6,7 @@
 /*   By: bmota-si <bmota-si@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/29 17:30:05 by mde-sa--          #+#    #+#             */
-/*   Updated: 2024/02/06 16:26:09 by bmota-si         ###   ########.fr       */
+/*   Updated: 2024/02/14 15:58:23 by bmota-si         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -66,9 +66,8 @@ char	*get_env_var_value(t_env *envv, char *var)
 			break ;
 		}
 	}
-	if (!value)
-		return (NULL);
-	get_env_var_value_utils(envv, var, &value, i);
+	if (value)
+		get_env_var_value_utils(envv, var, &value, i);
 	return (value);
 }
 
