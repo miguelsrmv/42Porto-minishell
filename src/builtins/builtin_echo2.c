1d304da55af26e9ef45ee0333dfd9cabac964ce5
Miguel <miguel.srmv@gmail.com>
2024-03-04 18:07:34 +0000

    Echo fixed

--- src/builtins/builtin_echo2.c
+++ src/builtins/builtin_echo2.c
@@ -6,7 +6,7 @@
 /*   By: mde-sa-- <mde-sa--@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/18 17:49:43 by bmota-si          #+#    #+#             */
-/*   Updated: 2024/03/04 17:50:09 by mde-sa--         ###   ########.fr       */
+/*   Updated: 2024/03/04 18:07:06 by mde-sa--         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,23 +18,16 @@ int	print_echo(char **args, bool print_newline, int i)
 	{
 		if (ft_strcmp(args[i], "") == 0)
 			;
-		else if (!args[i])
-		{
-			ft_putstr_fd(args[i], STDERR_FILENO);
+		else if (!printf("%s", args[i]))
 			return (EXIT_FAILURE);
-		}
 		else if (ft_strlen(args[i]) != 0 && args[i + 1] != NULL)
-		{
-			ft_putstr_fd(" ", STDERR_FILENO);
-			return (EXIT_FAILURE);
-		}
+			if (!printf(" "))
+				return (EXIT_FAILURE);
 		i++;
 	}
 	if (print_newline)
-	{
-		ft_putstr_fd("\n", STDERR_FILENO);
-		return (EXIT_FAILURE);
-	}
+		if (!printf("\n"))
+			return (EXIT_FAILURE);
 	return (EXIT_SUCCESS);
 }
 
