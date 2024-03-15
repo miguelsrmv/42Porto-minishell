a1567b118f08bda9e0e155860da76d0224c9357e
Miguel <miguel.srmv@gmail.com>
2024-03-15 19:54:46 +0000

    Quote Error

--- src/exit_error.c
+++ src/exit_error.c
@@ -6,7 +6,7 @@
 /*   By: mde-sa-- <mde-sa--@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/26 16:35:04 by mde-sa--          #+#    #+#             */
-/*   Updated: 2024/03/06 10:21:20 by mde-sa--         ###   ########.fr       */
+/*   Updated: 2024/03/15 19:54:26 by mde-sa--         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -61,7 +61,8 @@ void	set_g_status_flag(char *error_msg)
 		|| !ft_strcmp(error_msg, EMPTY_ERROR)
 		|| !ft_strcmp(error_msg, DIRECTORY_OUTPUT_ERROR))
 		g_status_flag = 1;
-	else if (!ft_strcmp(error_msg, SYNTAX_ERROR))
+	else if (!ft_strcmp(error_msg, SYNTAX_ERROR)
+		|| !ft_strcmp(error_msg, QUOTE_ERROR))
 		g_status_flag = 2;
 	else if (!ft_strcmp(error_msg, PERMISSION_ERROR)
 		|| !ft_strcmp(error_msg, DIRECTORY_ERROR))
